45b745fd673fec4acb5ca63c0a144af65544e39c
521masong <2605242386@qq.com>
2020-03-10 20:22:51 +0800

    add date 0310

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/OrderService.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/OrderService.java
@@ -1,12 +1,13 @@
 package io.cjf.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.cjf.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.cjf.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.cjf.jcartadministrationback.dto.out.OrderShowOutDTO;
 
 public interface OrderService {
 
-    Page<OrderListOutDTO> search(Integer pageNum);
+    Page<OrderListOutDTO> search(OrderSearchInDTO orderSearchInDTO, Integer pageNum);
 
     OrderShowOutDTO getById(Long orderId);
 
