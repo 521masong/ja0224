08066c17f10357dbd84c8fa311c4b1ae9c6b0e12
521masong <2605242386@qq.com>
2020-03-13 20:50:33 +0800

    add date 0313

--- pp/viewmodels/menu-navigate.js
+++ pp/viewmodels/menu-navigate.js
@@ -8,7 +8,7 @@ var app = new Vue({
                 index: "1",
                 icon: "el-icon-goods",
                 menuItems: [
-                    { name: "商品列表", index: "1-1" }
+                    { name: "商品列表",index: '1-1', route: '/product/search' }
                 ]
             },
             {
@@ -16,7 +16,7 @@ var app = new Vue({
                 index: "2",
                 icon: "el-icon-s-custom",
                 menuItems: [
-                    { name: "客户列表", index: "2-2" }
+                    { name: "客户列表", index: '2-2', route: '/customer/search' }
                 ]
             },
             {
@@ -24,8 +24,8 @@ var app = new Vue({
                 index: "3",
                 icon: "el-icon-s-order",
                 menuItems: [
-                    { name: "订单列表", index: "3-3" },
-                    { name: "退货列表", index: "3-4" }
+                    { name: "订单列表",  index: '3-3', route: '/order/search' },
+                    { name: "退货列表", index: '3-4', route: '/return/search' }
                 ]
             },
             {
@@ -33,15 +33,13 @@ var app = new Vue({
                 index: "4",
                 icon: "el-icon-user",
                 menuItems: [
-                    { name: "用户列表", index: "4-5" }
+                    { name: "用户列表",index: '4-5', route: '/administrator/index'  }
                 ]
             }
         ]
     },
     methods: {
-        handleMenuItemSelect(index, indexPath) {
-            console.log('menu item selected', index, indexPath);
-            this.selectMainPage = index;
-        }
+        
     }
+    
 })
